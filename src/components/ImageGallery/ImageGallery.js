91772d3eb3476fa5f56bc4ed7768cdef5c24cc51
T-Dmutro <90419458+T-Dmutro@users.noreply.github.com>
2023-05-02 11:09:22 +0300

    add component

---
+++ src/components/ImageGallery/ImageGallery.js
@@ -0,0 +1,9 @@
+import {ImageGalleryItem} from '../ImageGalleryItem/ImageGalleryItem.js';
+
+export const ImageGallery = ( ) =>{
+  return(
+    <ul>
+      <ImageGalleryItem/>
+    </ul>
+  )
+}
---
+++ src/components/Searchbar/Searchbar.js
@@ -0,0 +1,20 @@
+
+export const Seachbar =() =>{
+  return(
+    <header >
+  <form >
+    <button type="submit" >
+      <span >Search</span>
+    </button>
+
+    <input
+
+      type="text"
+      autoComplete="off"
+      autoFocus
+      placeholder="Search images and photos"
+    />
+  </form>
+</header>
+  )
+}
